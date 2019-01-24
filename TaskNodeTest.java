18e334a11f739761024ccea89e10d57114af358c
Karen K <41897864+karenkuri@users.noreply.github.com>
2019-01-23 19:22:59 -0700

    Add files via upload
    a test file based on what Isaac uploaded - Jan 23rd

---
+++ TaskNodeTest.java
@@ -0,0 +1,114 @@
+package assignment1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskNodeTest {
+
+	@Test
+	public void testTaskNodeStringStringTaskNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTaskNodeString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddInvalidNeighboursString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddInvalidNeighboursArrayListOfString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPenalty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInvalidNeighbours() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMachine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPenalty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
